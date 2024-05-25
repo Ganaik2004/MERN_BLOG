26a8a9ce8808e748fcbd81a3729f2d8dc25bf20d
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2024-05-25 16:57:41 +0530

    Connect the mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to the mongoDB")
+}).catch((err)=>{
+    console.log(err)
+})
 const app = express();
 app.use(express.json());
 const port = 3000;
