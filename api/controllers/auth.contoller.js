4a1ff959b0835448cfa1e100b75b4dda8721a7af
Ganaik2004 <137753416+Ganaik2004@users.noreply.github.com>
2024-05-26 19:09:02 +0530

    Crested the signin api route

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
   if (
@@ -26,3 +27,30 @@ export const signup = async (req, res,next) => {
    return next(err);
   }
 };
+export const signin = async(req,res,next)=>{
+  const {email,password } = req.body;
+  if(!email || !password || email===''||password===''){
+    return next(errorHandler(400,"Please Enter All The Feilds"));
+  }
+  try{
+   const validuser = await User.findOne({email});
+   if(!validuser){
+    return next(errorHandler(404,"User Not Found"));
+    }
+    const validPassword = bcryptjs.compareSync(password,validuser.password);
+    if(!validPassword){
+      return next(errorHandler(401,"Invalid Credential"));
+    }
+    const token = jwt.sign(
+      {id:validuser._id},
+      process.env.SECRET_KEY,
+      );
+      const {password:pass,...rest} = validuser._doc;
+
+res.status(200).cookie('access_token',token,{
+  httpOnly:true,
+}).json(rest);
+  }catch(err){
+    next(err);
+  }
+}
